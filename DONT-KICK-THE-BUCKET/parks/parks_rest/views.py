a723bb96465d75d81306ad9baf3c833b674e8d5b
Liam <liamecahill@gmail.com>
2022-07-14 18:30:36 -0400

    Merged branches for Parks and Users

--- DONT-KICK-THE-BUCKET/parks/parks_rest/views.py
+++ DONT-KICK-THE-BUCKET/parks/parks_rest/views.py
@@ -1,3 +1,19 @@
+from json import JSONDecoder
+from selectors import EpollSelector
+from urllib.robotparser import RequestRate
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from .models import Park
+import json
 # Create your views here.
+@require_http_methods(["GET", "POST"])
+def parks_list(request):
+    if request.method == "GET":
+        parks = Park.objects.all()
+        return JsonResponse(
+            {"parks": parks},
+            encoder=ParkEncoder,
+        )
+    else:
+        response = JsonResponse
