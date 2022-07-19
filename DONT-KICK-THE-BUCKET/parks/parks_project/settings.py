0a47ad804269bad10972c7081de8414b3e2a8ced
Liam <liamecahill@gmail.com>
2022-07-19 19:57:48 -0400

    completed poller.py, successfully hid API Key. currently working as intended.

--- DONT-KICK-THE-BUCKET/parks/parks_project/settings.py
+++ DONT-KICK-THE-BUCKET/parks/parks_project/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import dj_database_url
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
