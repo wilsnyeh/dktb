91d504ac615befde3ab8362f48642e5d8d56114d
Liam <liamecahill@gmail.com>
2022-07-20 14:59:58 -0400

    started writing login.js and routing it to app

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './Nav';
+import login from './users/login';
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
+        <Routes>
+          {/* <Route path="/" element={<MainPage/>} /> */}
+          
+          <Route path="/login" element={<login/>} />
+
+        </Routes>
 
 
       </div>
--- ghi/src/users/login.js
+++ ghi/src/users/login.js
@@ -1,31 +1,86 @@
-async function login(username, password) {
-    const url = `${process.env.api_user_token}/login/`;
+import React from 'react';
+import { Navigate, Link } from 'react-router-dom';
+import './login.css';
 
-const form = new FormData();
-  form.append("username", username);
-  form.append("password", password);
 
-  const response = await fetch(url, {
-    method: "post",
-    credentials: "include",
-    body: form,
-  });
-  if (response.ok) {
-    const url = `${process.env.api_user_token}/api/tokens/mine/`;
-
-    try {
-        const response = await fetch(tokenUrl, {
-          credentials: "include",
-        });
-        if (response.ok) {
-          const data = await response.json();
-          const token = data.token;
-          // DO SOMETHING WITH THE TOKEN SO YOU CAN USE IT
-          // IN REQUESTS TO YOUR NON-ACCOUNTS SERVICES
-        }
-      } catch (e) {}
-      return false;
+class LoginForm extends React.Component{
+    state={
+        username:'',
+        password:'',
+        showPassword: false,
+        error: '',
     }
-    let error = await response.json();
-    // DO SOMETHING WITH THE ERROR, IF YOU WANT
+
+    handleChange = (e) =>{
+        const {name, value} = e.target
+        this.setState({[name]: value})
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault()
+        const error = await this.props.login(this.state.username, this.state.password);
+        this.setState({ error: error })
+        this.setState({
+            username: '',
+            password: '',
+            showPassword: false,
+        });
+    }
+
+    validForm() {
+        return this.state.password.length >= 8 &&
+               this.state.username
+    }
+    render(){
+
+        if (this.props.token) {
+            return <Navigate to="/my_profile" />;
+        }
+
+        return(
+    
+            <div className="row_login" >
+                <div className="offset-3 col-6" id="wrapper" >
+                    <div className="shadow p-4 mt-4" id="outbox" >
+                        <form onSubmit={this.handleSubmit} id="login-form" >
+                            <h1>Ready to not kick that bucket?!</h1>
+                            <hr/>   
+                        
+                            <div className="form-floating mb-3">
+                                <input type='text' id="loginusername" name='username' placeholder='Username' 
+                                    required onChange={this.handleChange} />
+                            </div>
+                            <div className="form-floating mb-3" >
+                                <input type={this.state.showPassword ? "text" : "password"} id="loginpagepass" name='password' placeholder='●●●●●●●●' 
+                                    required onChange={this.handleChange} />
+                                    
+                                <button className='input-group-text bg-dark text-light' type="button"
+                                    onClick={() => this.setState({showPassword: !this.state.showPassword})}>Show Password
+                                </button>
+                            </div>
+                            <hr/>
+                            <div>
+                                <button 
+                                    type="submit" name='loginbutton'
+
+                                    className="btn btn-primary" form="login-form">Log In
+                                </button>
+                                <Link to='/login/new'>Forgot Password?</Link> 
+                            </div>
+                                
+                             
+                                <p>
+                                    Don't have an Account? <Link to='/login/new'>Create Account</Link> 
+                                </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+       
+        )
+    }
+       
 }
+
+
+export default LoginForm
