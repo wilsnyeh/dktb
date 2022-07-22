0f2d53e342d9afdd7bfe3e8fbc3fb5c5d6969c46
wilson <wilsnyeh@gmail.com>
2022-07-22 15:08:22 -0700

    updated branches

--- ghi/src/users/login.js
+++ ghi/src/users/login.js
@@ -1,44 +1,44 @@
 import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
-import {useToken} from "../Auth"
+import { useToken } from "../Auth"
 
 
 function Login(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const { token, login } = props;
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const { token, login } = props;
 
-//   if (login) {
-//     return <Navigate to='/' />;
-//   }  
+    //   if (login) {
+    //     return <Navigate to='/' />;
+    //   }  
 
-  var handleUserName = function (e) {
-    const value = e.target.value;
-    setUsername(value)
-    // props.setUN(value)
-  }
+    var handleUserName = function (e) {
+        const value = e.target.value;
+        setUsername(value)
+        // props.setUN(value)
+    }
 
-  return (
-    <form className='container mt-5 py-5'>
+    return (
+        <form className='container mt-5 py-5'>
             <input
-              onChange={handleUserName}
-              value={username}
-              placeholder='Username'
-              type='text'
-              required
+                onChange={handleUserName}
+                value={username}
+                placeholder='Username'
+                type='text'
+                required
             />
             <input
-              onChange={e => setPassword(e.target.value)}
-              value={password}
-              placeholder='Password'
-              type='password'
-              required
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+                placeholder='Password'
+                type='password'
+                required
             />
             <button onClick={() => login(username, password)} type='button'>
-              Login
+                Login
             </button>
-    </form>
-  );
+        </form>
+    );
 }
 
 export default Login;
