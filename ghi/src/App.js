22caa5d63e7ba23ba32fc98595edccb471bb79e1
Greg Robertson <grobertson743@gmail.com>
2022-08-01 23:08:20 -0700

    working on favorite feature

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,17 +2,26 @@
 import './App.css';
 import LoginForm from './users/login';
 import SignUpForm from './users/signup';
-import React from 'react';
 import Nav from "./Nav";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './mainpage/Header';
 import Footer from "./mainpage/Footer";
 import ParksList from './parks/ParksList';
 import ParkDetails from "./parks/ParkDetails"
+import AddFavorite from './parks/AddFavorites';
+import React, { useEffect, useState } from 'react'
 
 
 
 function App(props) {
+
+  const [favorites, setFavorites] = useState([])
+
+  const addFavoritePark = (park) => {
+    const newFavoriteList = [...favorites, park]
+    setFavorites(newFavoriteList);
+  }
+
   return (
     <BrowserRouter>
       <Nav />
@@ -20,7 +29,7 @@ function App(props) {
       <Routes>
         <Route>
           <Route path="/" element={<ParksList fetchUrl={`${process.env.REACT_APP_PARKS}/parks/list/`} />} />
-          <Route path="parks/:id" element={<ParkDetails detailUrl={`${process.env.REACT_APP_PARKS}/parks/list/`} />} />
+          <Route path="parks/:id" element={<ParkDetails detailUrl={`${process.env.REACT_APP_PARKS}/parks/list/`} handleFavoriteClick={addFavoritePark} favoriteComponent={AddFavorite} />} />
         </Route>
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignUpForm />} />
--- ghi/src/parks/ParkDetails.js
+++ ghi/src/parks/ParkDetails.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import id from "./ParksList"
+import AddFavorite from './AddFavorites'
+// import id from "./ParksList;
+
+
 
 const parkUrl = "http://localhost:8080/parks/list/"
 
 function ParkDetails({ detailUrl, ...props }) {
   const [park, setPark] = useState({})
+
   const { id } = useParams()
+  const FavoriteComponent = props.favoriteComponent
+  const HandleFavoritesClick = props.handleFavoritesClick
   console.log(id)
   useEffect(() => {
     async function fetchData() {
@@ -23,15 +29,17 @@ function ParkDetails({ detailUrl, ...props }) {
     <div className='parks-list'>
       <div key={park.id} className="row">
         <div className="col-5 text-center mx-auto">
-          <h2 className="featurette-heading">{park.name}</h2>
+          <h2 className="featurette-heading">{park.name} {AddFavorite}</h2>
           <h4><span className="text-muted">{"   " + park.city + ", " + park.state}</span></h4>
           <p className="lead">{park.description}</p>
         </div>
-        <div className="text-center">
-          <img className="img-fluid mx-auto"
-            src={park.image_url} alt="" />
+        <div className="image-container">
+          <img className="img-fluid mx-auto" src={park.image_url} alt="" />
+          <div onClick={HandleFavoritesClick} className='overlay align-items-center justify-content'>
+            <FavoriteComponent />
+          </div>
+          <hr className="featurette-divider" />
         </div>
-        <hr className="featurette-divider" />
       </div>
     </div>
   )
