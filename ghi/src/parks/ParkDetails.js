0c3eb044d71756f544c36585b1e0015816e214ac
Liam <liamecahill@gmail.com>
2022-08-03 17:32:15 -0400

    currently still working on pulling sergeys park list and park detail page.

--- ghi/src/parks/ParkDetails.js
+++ ghi/src/parks/ParkDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import "../DetailPage.css"
 import Header from '../mainpage/Header'
 
 
@@ -23,12 +24,11 @@ function ParkDetails({ detailUrl, weatherUrl, ...props }) {
   useEffect(() => {
     if (Object.keys(park).length) {
       async function fetchData() {
-        const Url = weatherUrl + park.city + "," + park.state + ",US&appid=" + apiKey + '&units=metric';
+        const Url = weatherUrl + park.city + "," + park.state + ",US&appid=" + apiKey + '&units=imperial';
         const response = await fetch(Url);
         const data = await response.json();
         // setWeather(data.main);
         setWeather(data.main);
-        console.log(data);
         return response;
       }
       fetchData();
@@ -36,29 +36,32 @@ function ParkDetails({ detailUrl, weatherUrl, ...props }) {
   }, [park, weatherUrl])
 
   return (
-    <div> <Header />
-      <div className='parkslist'>
-        <div key={park.id} className="row">
-          <div className="col-9">
-            <h2 className="featurette-heading">{park.name}</h2>
-            <h4><span className="text-muted">{"   " + park.city + ", " + park.state}</span></h4>
-            <p className="lead">{park.description}</p>
-          </div>
-          <div className="col-3 photo">
-            <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto parksphoto"
-              src={park.image_url} alt="" />
-          </div>
-          <hr className="featurette-divider" />
+    <div className='parkdetail'>    
+      <div key={park.id} className="row">
+        <div className="col-12">
+          <h2 className="featurette-heading-detail">{park.name}</h2>
+          <p className="lead">{park.description}</p>
         </div>
-        <div>
-          {weather
-            ? <div><h1>Temperature: {weather?.temp} C</h1><h1>Humidity: {weather?.humidity}%</h1> </div>
-            : <></>
-          }
+        <div className="col-12 photo">
+          <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto parksphoto"
+            src={park.image_url} alt="" />
         </div>
       </div>
+      <div>
+      {weather
+        ? <div><h5>Weather in {park.city} now: </h5><p>Temperature: {weather?.temp} °F</p><p>Humidity: {weather?.humidity}%</p> </div>
+        : <></>          
+      }
+      </div>
+      <div className="col-12">
+        {park.weather_info}
+      </div>
+      <div className='row row-details'>
+        <div className="col-6">Entrance fee: {park.entrance_fee}</div>
+        <div className="col-6">Contact number: {park.contact_num}</div>
+      </div>
+      
     </div>
   )
 }
 export default ParkDetails
-
