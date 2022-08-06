a97338ba17093afca2c256e7faaee883caf86b55
wilson <wilsnyeh@gmail.com>
2022-08-05 17:40:01 -0700

    grabbed siahs code for login/signup, still working

--- ghi/src/users/login.js
+++ ghi/src/users/login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useToken } from "../Auth"
+import "../HomePage.css"
 
 
 function Login() {
@@ -14,53 +15,56 @@ function Login() {
         navigate(path);
     }
     return (
-        <section className='w-100 d-flex justify-content-center pb-4'>
-            <div className='form-container'>
-                <h1 className='p-4 m-auto pb-0 text-light'>Login to your Account</h1>
-                <form className='container w-80 mt-2 py-1'>
-                    <div className='form-outline form-outline-color-success my-4 mx-3'>
-                        <label className='form-label' htmlFor='username'>Username</label>
-                        <input
-                            className='form-control '
-                            onChange={e => setUsername(e.target.value)}
-                            value={username}
-                            placeholder='Username'
-                            type='text'
-                            required
-                        />
+        <div className="hero vh-100 d-flex align-items-center">
+            <div className='container'>
+                <span className="font-link">
+                    <div className="row"></div>
+                    <h1 className='p-4 m-auto pb-0 text-light'>Login to your Account</h1>
+                    <form className='container w-80 mt-2 py-1'>
+                        <div className='form-outline form-outline-color-success my-4 mx-3'>
+                            <label className='form-label' htmlFor='username'><font size="+2">Username</font></label>
+                            <input
+                                className='form-control '
+                                onChange={e => setUsername(e.target.value)}
+                                value={username}
+                                placeholder='Username'
+                                type='text'
+                                required
+                            />
+                        </div>
+                        <div className='my-4 mx-3'>
+                            <label className='form-label' htmlFor='password'><font size="+2">Password</font></label>
+                            <input
+                                className='form-control'
+                                onChange={e => setPassword(e.target.value)}
+                                value={password}
+                                placeholder='Password'
+                                type='password'
+                                required
+                            />
+                        </div>
+                        <div className='container-fluid flex'>
+                            <button
+                                className='btn btn-success float-end' onClick={() => login(username, password)} type='button'>
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                    <div className='row mx-2 m-auto'>
+                        <div className='col m-auto'>
+                            <p>Don't have an account?</p>
+                        </div>
+                        <div className='col mb-1'>
+                            <button
+                                onClick={routeChange}
+                                className="btn btn-success btn-block">
+                                Sign up
+                            </button>
+                        </div>
                     </div>
-                    <div className='my-4 mx-3'>
-                        <label className='form-label' htmlFor='password'>Password</label>
-                        <input
-                            className='form-control'
-                            onChange={e => setPassword(e.target.value)}
-                            value={password}
-                            placeholder='Password'
-                            type='password'
-                            required
-                        />
-                    </div>
-                    <div className='row mx-5 mb-4'>
-                        <button
-                            className='btn btn-success' onClick={() => login(username, password)} type='button'>
-                            Login
-                        </button>
-                    </div>
-                </form>
-                <div className='row mx-2 m-auto'>
-                    <div className='col m-auto'>
-                        <p>Don't have an account?</p>
-                    </div>
-                    <div className='col mb-1'>
-                        <button
-                            onClick={routeChange}
-                            className="btn btn-success btn-block">
-                            Sign up
-                        </button>
-                    </div>
-                </div>
+                </span>
             </div>
-        </section>
+        </div>
     );
 }
 export default Login;
