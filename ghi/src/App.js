0b37f8a1f5ec6b4082ddf67be516a10b77348e73
Greg Robertson <grobertson743@gmail.com>
2022-08-16 08:52:36 -0700

    cleaned up code on App.js, removed unecessary code

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -6,29 +6,29 @@ import homePage from './mainpage/Homepage'
 import Footer from "./mainpage/Footer";
 import ParksList from './parks/ParksList';
 import ParkDetails from "./parks/ParkDetails"
-import React, {  useState } from 'react'
+import React from 'react'
 import { useAuthContext, useToken } from './Auth'
 import Nav from './Nav';
 import AccountDetails from './users/AccountPage';
-import jwt from "jwt-decode";
+// import jwt from "jwt-decode";
 
 
 function App(props) {
   const [, , logout] = useToken();
-  const { token, decoded } = useAuthContext();  
+  const { token, decoded } = useAuthContext();
   const userId = decoded?.user?.id
-  const [favorites, setFavorites] = useState();
+  // const [favorites, setFavorites] = useState();
 
 
   return (
     <>
       <Nav logout={logout} token={token} />
       <Routes>
-        <Route> 
-          <Route path="/" element={homePage}/>
+        <Route>
+          <Route path="/" element={homePage} />
           <Route path="/parks" element={<ParksList fetchUrl={`${process.env.REACT_APP_PARKS}/parks/list/`} token={token} />} />
           <Route path="parks/:id" element={<ParkDetails userId={userId} detailUrl={`${process.env.REACT_APP_PARKS}/parks/list/`} weatherUrl={'https://api.openweathermap.org/data/2.5/weather?q='} token={token} />} />
-          <Route path="/account" element={<AccountDetails accountUrl ={`http://localhost:8090/accounts/${userId}/`}/>} />
+          <Route path="/account" element={<AccountDetails accountUrl={`http://localhost:8090/accounts/${userId}/`} />} />
         </Route>
         <Route path="/login" element={<LoginForm token={token} />} />
         <Route path="/signup" element={<SignUpForm />} />
