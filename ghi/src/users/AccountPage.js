98244087ac81074e66d425dd44d2f23f5ed96d8c
wilson <wilsnyeh@gmail.com>
2023-02-17 23:52:06 -0800

    user account list changes

--- ghi/src/users/AccountPage.js
+++ ghi/src/users/AccountPage.js
@@ -22,10 +22,13 @@ function AccountDetails({ accountUrl, updateFavorites, ...props }) {
             <div>
                 <span></span>
                 <div className='m-5'>
+                    
                     <h1 className='pt-4'>{favorites.email}</h1>
-                    <div style={{ justifyContent: "space-around" }} className='row row-cols-2 row-cols-sm-1 row-cols-md-3 row-cols-lg-4 g-1'>
+                    <div> TESTING </div>
+                    
+                    <div  style={{ justifyContent: "space-around" }} className=''>
                         {favorites.parks
-                            ? <div >{favorites.parks.map((park) => {
+                            ? <div className='list-column'>{favorites.parks.map((park) => {
                                 return (
                                     <div key={park.id} className='card mb-5 mx-2 shad_bottom off_white list-border'>
                                         <div className="col">
@@ -37,17 +40,20 @@ function AccountDetails({ accountUrl, updateFavorites, ...props }) {
                                                     </label>
                                                 </div>
                                                 <h3 className="card-title ">{park.name}</h3>
+                                                <h4 className="card-subtitle mb-2 text-muted">{park.city}</h4>
                                                 <h4 className="card-subtitle mb-2 text-muted">{park.state}</h4>
                                                 <img className="img-fluid list-border-inner "
                                                     src={park.image_url} alt="national park" />
                                             </div>
                                         </div>
                                     </div>
+
                                 )
                             })}</div>
                             : null
                         }
                     </div>
+                    
                 </div>
             </div>
         </>
